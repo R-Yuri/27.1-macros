60f39717f93490927a155344d467c29bf51ddd0e
Yuri <Yuri@DESKTOP-0A9TFBL>
2023-09-14 19:27:17 +0300

    Добавьте файлы проекта.

---
+++ 27.1 macros/27.1 macros.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+#define DAY_OF_WEEK(n) \
+   (n == 1 ? "Monday" : \
+    n == 2 ? "Tuesday" : \
+    n == 3 ? "Wednesday" : \
+    n == 4 ? "Thursday" : \
+    n == 5 ? "Friday" : \
+    n == 6 ? "Saturday" : \
+    n == 7 ? "Sunday" : \
+    "?")
+
+#define PRINT_DAY(day) \
+    std::cout << DAY_OF_WEEK(day) << std::endl;
+
+int main() {
+    int day;
+    std::cout << "Enter the day of the week as its ordinal number (1-7): ";
+    std::cin >> day;
+    PRINT_DAY(day);
+    return 0;
+}
